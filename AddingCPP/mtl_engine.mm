feb9406fe87da3f7e1c0ef1274bafbb4b33b0ea1
GeraldJones <gajone12@asu.edu>
2025-05-04 15:47:50 -0700

    Created my first triangle in metal-cpp. Fuzzy edges but still very cool. Properly used a render pipeline for it

--- AddingCPP/mtl_engine.mm
+++ AddingCPP/mtl_engine.mm
@@ -99,3 +99,37 @@ void MTLEngine::createRenderPipeline() {
     
     renderPipelineDescriptor->release();
 }
+
+void MTLEngine::draw() {
+    sendRenderCommand();
+}
+
+void MTLEngine::sendRenderCommand() {
+    metalCommandBuffer = metalCommandQueue->commandBuffer();
+    
+    MTL::RenderPassDescriptor* renderPassDescriptor = MTL::RenderPassDescriptor::alloc()->init();
+    MTL::RenderPassColorAttachmentDescriptor* cd = renderPassDescriptor->colorAttachments()->object(0);
+    cd->setTexture(metalDrawable->texture());
+    cd->setLoadAction(MTL::LoadActionClear);
+    cd->setClearColor(MTL::ClearColor(41.0f/255.0f, 42.0f/255.0f, 48.0f/255.0f, 1.0f));
+    cd->setStoreAction(MTL::StoreActionStore);
+    
+    MTL::RenderCommandEncoder* renderCommandEncoder = metalCommandBuffer->renderCommandEncoder(renderPassDescriptor);
+    encodeRenderCommand(renderCommandEncoder);
+    renderCommandEncoder->endEncoding();
+    
+    metalCommandBuffer->presentDrawable(metalDrawable);
+    metalCommandBuffer->commit();
+    metalCommandBuffer->waitUntilCompleted();
+    
+    renderPassDescriptor->release();
+}
+
+void MTLEngine::encodeRenderCommand(MTL::RenderCommandEncoder* renderCommandEncoder) {
+    renderCommandEncoder->setRenderPipelineState(metalRenderPSO);
+    renderCommandEncoder->setVertexBuffer(triangleVertexBuffer, 0, 0);
+    MTL::PrimitiveType typeTriangle = MTL::PrimitiveTypeTriangle;
+    NS::UInteger vertexStart = 0;
+    NS::UInteger vertexCount = 3;
+    renderCommandEncoder->drawPrimitives(typeTriangle, vertexStart, vertexCount);
+}
